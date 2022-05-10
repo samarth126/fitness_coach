7334c3d392319c4c10dcd692ed5275608e316731
priyanshu parashar <priyanshuparashar223@gmail.com>
2022-05-10 10:45:46 +0530

    final push

--- fitness_coach/base/urls.py
+++ fitness_coach/base/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
 
     path('',views.home, name="home"),
 
-    path('cool/',views.cool, name="cool"),
+    path('workout/',views.cool, name="cool"),
     path('cam_but/',views.cam_but, name="cam_but"),
 
     path('logout/', views.logoutUser, name="logout"),
@@ -15,14 +15,16 @@ urlpatterns = [
     path('profile/', views.profilee, name='profile'),
 
     path('health/', views.health, name='health'),
-    path('noti/', views.noti, name='noti'),
+    path('notification/', views.noti, name='noti'),
     path('contact/', views.contactt, name='contactt'),
 
     path('food/', views.food1, name='food'),
     path('pd_u/', views.pd_u, name='pd_u'),
     path('h_w/', views.h_w, name='h_w'),
+    path('bmi_calc/', views.bmi_calc, name='bmi_calc'),
 
     path('result/', views.result, name='result'),
     path('web/', views.web, name='web'),
+    path('tips/', views.tips, name='tips'),
 
 ]
